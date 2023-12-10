75d578a553bf327e62a5bca562da3110fe1df728
Abhi <abhisadineni@gmail.com>
2023-12-10 15:18:19 +0530

    made changes to cart model

--- project/models/cart_model.py
+++ project/models/cart_model.py
@@ -7,6 +7,7 @@ class Cart(db.Model):
     book_id = db.Column(db.Integer,db.ForeignKey('book.id'),nullable = False)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable = False)
     quantity = db.Column(db.Integer,nullable=False,default = 1)
+    saved_for_later = db.Column(db.Boolean, default = False)
     created_date_time = db.Column(db.DateTime, default=datetime.datetime.now())
     last_updated = db.Column(db.DateTime, default=datetime.datetime.now())
     last_updated_by = db.Column(db.Integer,db.ForeignKey('user.id'))
